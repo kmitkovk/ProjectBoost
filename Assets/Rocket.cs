038415406ba84ec7eed6b77d2304da7761c00319
Krasimir Kerliev <krasimir.kerliev@gen-i.si>
2020-12-28 11:46:18 +0100

    Add some styles improve flight bugs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,41 +4,87 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-
+    //TODO TIME.DELTATIME
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    [SerializeField] float rcsThrust = 200f;
+    [SerializeField] float mainThrust = 300f;
 
 
     void Start()
     {
 
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput()
+    void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space)) 
+        if (Input.GetKey(KeyCode.Space))
         {
             ///print("Thursting"); // Can thurst while rotating thats while blow we have IF not else...its inclusinve
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying) //so it doesnt layer on top of each other..
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
 
-        if (Input.GetKey(KeyCode.D)) // here in the below we have else becase we want these exclusive
+    void Rotate()
+    {
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        // take manual control of the roatation (below)
+        // freeze physics rotatation before we take manual control of rotation
+        rigidBody.freezeRotation = true;
+        
+        if (Input.GetKey(KeyCode.A)) // here in the below we have else becase we want these exclusive
         {
             //print("Rotating Right");
-            transform.Rotate(-Vector3.forward);
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
-        else if (Input.GetKey(KeyCode.A)) // D takes presedence because it comes first
+        else if (Input.GetKey(KeyCode.D)) // A takes presedence because it comes first
         {
             //print("Rotating Left");
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //here we resume the physics control of rotation
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing on friendly objects
+                print("OK collision");
+                break;
+
+            case "Fuel":
+                //do nothing on friendly objects
+                print("OK collision");
+                break;
+
+            default:
+                print("DEAD"); // kill player
+                break;
         }
     }
 }
